a1f26c442e18bf1ad15c1c4fafb29d4b0b0b98c2
WMacharia <grace.macharia@strathmore.edu>
2024-10-02 08:30:58 +0300

    Declared variables

--- signup.php
+++ signup.php
@@ -1,3 +1,11 @@
 <?php
 require "load.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $genderId = $_POST['genderId'];
+    $roleId = $_POST['roleId'];
+}
 ?>
