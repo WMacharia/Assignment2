d7d615c620ac9175d30744cef0afe1328279327c
WMacharia <grace.macharia@strathmore.edu>
2024-10-05 17:39:23 +0300

    Added variable for fullname

--- signup.php
+++ signup.php
@@ -3,13 +3,13 @@ require "load.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
+    $fullname = $_POST['fullname'];
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $genderId = $_POST['genderId'];
 
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO userinfo (email, username, password, genderId) VALUES ('$email', '$username', '$hashed_password', '$genderId')";
+    $sql = "INSERT INTO userinfo (email, FullName, username, password) VALUES ('$email', '$fullname', '$username', '$hashed_password')";
 
     if ($conn->query($sql) === TRUE) {
         echo "USER REGISTERED SUCCESFULLY!"; 
