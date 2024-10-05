5f9b5a4d80e8d3f90e2718d1c0a7766a1891c0ec
WMacharia <grace.macharia@strathmore.edu>
2024-10-05 22:29:55 +0300

    Created Instances

--- load.php
+++ load.php
@@ -2,10 +2,11 @@
 require "db/constants.php";
 require "db/dbconn.php";
 require "forms/signupform.php";
+require "forms/signinform.php";
 
 $db = new dbConnection(DBTYPE , HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
 $conn = $db->connection;
 
 $objform = new signupform();
-
+$objsignin = new signinform();
 ?>
