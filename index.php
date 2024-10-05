a90970f348cf1582931014c7a19d85316dcbcdde
WMacharia <grace.macharia@strathmore.edu>
2024-10-05 17:43:11 +0300

    Modified

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-require_once "load.php";  // Make sure this file correctly initializes the DB connection
+require_once "load.php";  
 
-// Initialize the signupform object
 $objsignupform = new signupform();
-
-// Call the sign_up_form method
 $objsignupform->sign_up_form($conn);
