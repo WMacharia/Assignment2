91d0176858b1edd220fb7302f63ac99b3fa67ed5
WMacharia <grace.macharia@strathmore.edu>
2024-11-16 11:56:19 +0300

    Updated MySQL Port

--- db/constants.php
+++ db/constants.php
@@ -2,7 +2,7 @@
 
 define('DBTYPE', 'PDO');
 define('HOSTNAME', 'localhost');
-define('DBPORT', '3308');
+define('DBPORT', '3309');
 define('HOSTUSER', 'root');
 define('HOSTPASS', '');
 define('DBNAME', 'assignment2');
